78062904c7cbd93d067f96deedebde9ed707a6ed
John Henry Galino <github.stolze@simplelogin.com>
2023-11-06 14:04:51 +0800

    add shuffle and draw methods to Deck

--- card.ts
+++ card.ts
@@ -4,3 +4,34 @@ class Card {
         private tokens: number = 0,
     ) {}  
 }
+
+class Collection {
+    constructor(
+        protected cards: Card[] = []
+    ) {}
+}
+
+class Deck extends Collection {
+    constructor() {
+        super();
+    }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+          }
+    }
+
+    draw() {
+        return this.cards.pop();
+    }
+
+    get length() {
+        return this.cards.length;
+    }
+
+    static Generate(min, max: number) {
+        
+    }
+}
