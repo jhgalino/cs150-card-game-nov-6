5fd3c561839c1bd17f4a98e8651420e01e62409b
jproads <jp.roads03@gmail.com>
2023-11-06 14:37:37 +0800

    feat: add more score and simulate logic

--- simulate.ts
+++ simulate.ts
@@ -1,5 +1,5 @@
 import { Card, Deck } from './card.ts';
-import { Game, GameState, Move } from './game.ts';
+import { Game, GameState } from './game.ts';
 
 function simulate(game: Game) {
     // Game loop
