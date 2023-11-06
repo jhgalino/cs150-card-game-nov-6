cd897a3a6864a69176509d67aa4863c574d950c0
jproads <jp.roads03@gmail.com>
2023-11-06 14:20:28 +0800

    feat(game.ts): flesh out nextRound

--- game.ts
+++ game.ts
@@ -2,26 +2,46 @@ import { Card, Deck } from './card.ts';
 import { Player } from './player.ts';
 import { xorRNG } from './random.ts';
 
-class Game {
+enum GameState {
+    RUNNING = "run",
+    END = "end",
+}
+
+export class Game {
     constructor(
         private deck: Deck,
         private players: Player[],
-        private round: number = 1, // 1-indexed
+        private turn: number = 0,
         private currentPlayer: number = 0, // Index of the players array
-        private currentCard: Card = null,
+        private currentCard: Card | null = null, // No current card at start
+        private state: GameState = GameState.RUNNING,
     ) {}
 
     nextRound() {
+        // Update state variables
+        this.turn++;
         const player = this.players[this.currentPlayer];
-        
-        if (xorRNG.randomInt(0, 2) && player.payToken()) { // Chooses to skip turn & can pay token
-            
-        } else { // Takes card
+        this.currentCard = this.deck.draw();
 
+        // Randomize player's move
+        if (xorRNG.randomInt(0, 2) && player.payToken()) { // Chooses to skip turn & can pay token
+            this.currentCard?.addToken();
+            this.nextCurrentPlayer();
+        } else { // Takes card
+            player.addToHand(this.currentCard!)
         }
     }
 
     nextCurrentPlayer() {
         this.currentPlayer = (this.currentPlayer + 1) % this.players.length
+    }
+
+    simulate() {
+        // Game loop
+        while 
+        // Check for game-ending conditions
+        if (this.deck.length === 0) this.endGame();
+
+        
     }
 }
