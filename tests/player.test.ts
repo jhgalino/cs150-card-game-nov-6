447db4c9b1a6914045dfec15b9fe668dc2cfacce
jproads <jp.roads03@gmail.com>
2023-11-06 15:18:12 +0800

    feat: add unit test

--- tests/player.test.ts
+++ tests/player.test.ts
@@ -3,12 +3,25 @@ import { Player } from '../player';
 import { Card } from '../card';
 
 test("Player successfully places a token on card", () => {
-    const testPlayer = new Player();
+    const testPlayer = new Player(11); // 11 tokens
     const testCard = new Card(3);
 
+    expect(testPlayer.payTokenTo(testCard)).toBe(1);
+
     testPlayer.payTokenTo(testCard);
+
+    expect(testPlayer.tokens).toBe(10);
+    expect(testCard.tokens).toBe(1);
 })
 
 test("Tokenless player tries to place token on card", () => {
-    const
+    const testPlayer = new Player(0);
+    const testCard = new Card(3);
+
+    expect(testPlayer.payTokenTo(testCard)).toBe(0);
+
+    testPlayer.payTokenTo(testCard);
+
+    expect(testPlayer.tokens).toBe(0);
+    expect(testCard.tokens).toBe(0);
 })
